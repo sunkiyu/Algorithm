4ff712f368c7a2b7ed2e547bf81c668caafde03f
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-01-07 17:16:28 +0900

    Update SeletionSort.cpp

--- SeletionSort.cpp
+++ SeletionSort.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
-//시간 복잡도 O(N^2)
-//최소값을 찾은 후 맨 앞으로 보낸다
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
+
 using namespace std;
+
+int dubple[1000];
 int main()
 {  
-   const int listSz=5;
-   int sSortlist[listSz] = {5, 8, 3, 2, 4};
-
-   for(int i=0; i<listSz; i++)
+   const int listSz=50;
+   
+   int sSortlist[listSz+1];
+   
+   srand((unsigned)time(NULL));
+   
+    for (int i = 1; i <= listSz; )
+    {
+        sSortlist[i] = rand() % 300 + 1;
+        if (dubple[sSortlist[i]] == false)
+        {
+            dubple[sSortlist[i]] = true;
+            i++;
+        }
+    }
+    
+    for(int i=1; i<=listSz; i++) cout<< sSortlist[i]<<' ';
+    cout<<endl;
+   for(int i=1; i<=listSz; i++)
    {
        int findMin = 2e9,minIdx = -1;
-       for(int j=i; j<listSz; j++)
+       for(int j=i; j<=listSz; j++)
        {
            if(findMin>sSortlist[j])
            {
@@ -21,6 +40,6 @@ int main()
        swap(sSortlist[i],sSortlist[minIdx]);
    };
    
-   for(int i=0; i<listSz; i++) cout<<sSortlist[i];
+   for(int i=1; i<=listSz; i++) cout<<sSortlist[i]<<' ';
 return 0;   
 }
